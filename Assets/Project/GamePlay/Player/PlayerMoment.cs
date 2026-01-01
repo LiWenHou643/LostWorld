a907865c67d606b3971e9eae5726e1038afa270f
LiWenHou643 <quannhan6969@gmail.com>
2026-01-01 23:44:56 +0700

    Feat: Add idle, walk animation

--- Assets/Project/GamePlay/Player/PlayerMoment.cs
+++ Assets/Project/GamePlay/Player/PlayerMoment.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class PlayerMoment : MonoBehaviour
 {
     public float speed = 5f;
+    public int facingDirection = 1; // 1: right, -1: left
     public Rigidbody2D rb;
+    public Animator animator;
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,6 +20,22 @@ public class PlayerMoment : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        if (horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        animator.SetFloat("Horizontal", Mathf.Abs(horizontal));
+        animator.SetFloat("Vertical", Mathf.Abs(vertical));
+
         rb.linearVelocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
